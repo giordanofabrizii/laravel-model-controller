acf8bf5268d31ac5a45a394c687a3795858971e9
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-17 20:29:02 +0200

    added film info

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,23 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ Vite::asset('resources/scss/app.scss') }}">
     <title>
         @yield('page-name')
     </title>
+    @vite('resource/js/app.js')
 </head>
 <body>
-    <header>
-        <h1>
-            Titolo del layout
-        </h1>
-    </header>
     <main>
         @yield('main-content')
     </main>
-    <footer>
-        <h1>
-            Titolo del layout
-        </h1>
-    </footer>
 </body>
 </html>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('main-content')
-
-@dump($movies)
+<section id="film-controller">
+    @foreach ($movies as $film)
+        <article>
+            <h1>{{ $film->title }}</h1>
+            <p>
+                Original name: {{ $film->original_title }}<br>
+                Nationality: {{ $film->nationality }}<br>
+                Date: {{ $film->date }}<br>
+                Vote: {{ $film->vote }}
+            </p>
+        </article>
+    @endforeach
+</section>
 
 @endsection
